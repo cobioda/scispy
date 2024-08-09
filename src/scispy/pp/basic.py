c439530f5f9d7d52803edde8ead6ec269fb271b0
MorganeFi <morgane.fierville@gmail.com>
2024-08-09 14:55:24 +0200

    modif integration functions pp/basic.py

--- src/scispy/pp/basic.py
+++ src/scispy/pp/basic.py
@@ -5,7 +5,9 @@ import scvi
 import spatialdata as sd
 import squidpy as sq
 from matplotlib import pyplot as plt
-
+import numpy as np
+import yaml
+import warnings
 
 def run_scanpy(
     sdata: 'sd.SpatialData',
@@ -75,6 +77,21 @@ def run_scanpy(
 
     # for vizgen previous analysis
     # sdata.shapes['cell_boundaries'] = sdata.shapes['cell_boundaries'].loc[sdata.table.obs.index.tolist()]
+    return
+
+def check_if_negative_coords(
+    sdata: 'sd.SpatialData'
+): 
+    """Check if some cells have negative coordinates.
+
+    Parameters
+    ----------
+    sdata
+        SpatialData object.
+    
+    Return
+    """
+    return np.any((sdata['table'].obs['center_x'] <= 0) | (sdata['table'].obs['center_y'] <= 0))
 
 
 def sync_shape(
@@ -97,16 +114,43 @@ def sync_shape(
     sdata[shape_key] = sdata[shape_key].loc[sdata['table'].obs[instance_key].tolist()]
 
 
-def scvi_annotate(
-    ad_spatial: an.AnnData,
-    ad_ref: an.AnnData,
-    label_ref: str = "celltype",
-    label_key: str = "celltype",
-    layer: str = "counts",
-    metaref2add: tuple = [],
-    filter_under_score: float = 0.5,
+def replaceGeneNames(
+    adata: 'an.AnnData',
+    path: 'str',
+    genes: 'set',
 ):
-    """Annotate anndata spatial cells using anndata cells reference using SCVI.
+    """Replace old gene name by the new gene name.
+
+    Parameters
+    ----------
+    adata
+        AnnData object.
+    path
+        path to YAML file with old gene name and new gene name.
+    genes
+        gene names to modifiy.
+    """
+    with open(path, 'r') as file:
+        genes_dict = yaml.safe_load(file)
+
+    for gene in genes:
+        if gene in genes_dict.keys():
+            adata.var = adata.var.rename(index={genes_dict[gene]: gene}) 
+        else:
+            warnings.warn(f' The new name of {gene} is not found. Please check if you find it on : "https://www.genecards.org/" and if so add it.')
+    return
+    
+    
+def prepareToScvi(
+    ad_spatial: 'an.AnnData',
+    ad_ref: 'an.AnnData',
+    label_key: 'str' = "celltype",
+    batch_categories: 'list' = ["reference", "query"],
+    unlabeled_category: 'str' = "unknown",
+    batch_key: 'str' = "tech",
+    path_to_renamed_genes = '/data/analysis/data_fierville/SPATIAL/BPCO/Human_GRCh38_renamed_genes.yaml'
+) -> 'an.AnnData':
+    """Prepare the two datasets to integrate by combining common genes and concatenating them.
 
     Parameters
     ----------
@@ -114,88 +158,203 @@ def scvi_annotate(
         Anndata spatial object.
     ad_ref
         Anndata single-cell reference object.
-    label_ref
-        .obs key in single-cell reference object.
     label_key
-        .obs key in spatial object.
+        .obs key in single-cell reference object and in spatial object.
+    metaref2add
+        .obs key in single-cell reference object to transfert to spatial.
+    path_to_renamed_genes
+        path to YAML file with old gene name and new gene name.
+
+    Returns
+    ----------
+    Anndata object with the two datasets concatenate.
+    """
+    # ad_spatial.var.index = ad_spatial.var.index.str.upper()
+    # ad_ref.var.index = ad_ref.var.index.str.upper()
+    
+    print("Reference:", ad_ref.n_vars, "genes")
+    print("Qurey:", ad_spatial.n_vars, "genes")
+    
+    # genes in spatial only
+    not_in_ref = set(ad_spatial.var_names) - set(ad_ref.var_names)
+    
+    if not_in_ref:
+        replaceGeneNames(ad_ref, path_to_renamed_genes, genes=not_in_ref)
+    else:
+        print('All spatial genes are in the single-cell dataset')
+    # print(set(ad_spatial.var_names) - set(ad_ref.var_names))
+    genes_shared = ad_spatial.var_names.intersection(ad_ref.var_names)
+    print(len(genes_shared), "common genes")
+
+    print("Concatenate both datasets...")
+    # both needs to have the count values in the layer "counts" or layer received
+    concat = ad_ref[:, genes_shared].concatenate(ad_spatial[:, genes_shared], 
+                                                 batch_key = batch_key, 
+                                                 batch_categories=batch_categories).copy()
+    concat.obs[label_key] = concat.obs[label_key].cat.add_categories([unlabeled_category])
+    concat.obs.loc[concat.obs[batch_key] == batch_categories[1], label_key] = unlabeled_category
+    
+    return concat
+
+
+def run_scANVI(
+    adata_train: 'an.AnnData',
+    layer: 'str' = "counts",
+    batch_key: 'str' = "tech",
+    label_key: 'str' = 'celltype',
+    n_layers: 'int' = 2,
+    n_latent: 'int' = 50,
+    unlabeled_category: 'str' = "unknown",
+) -> 'an.AnnData':
+    """Run scvi and scanvi and predict the annotation of unlabeled cells.
+
+    Parameters
+    ----------
+    adata_train
+        Anndata object.
     layer
         layer in which we can find the raw count values.
+    batch_key
+        .obs key in anndata object that separate the reference from the query.
+    label_key
+        .obs key in anndata object to make the annotation.
+    n_layers
+        number of hidden layers used for encoder and decoder NNs.
+    n_latent
+        dimensionality of the latent space.
+    unlabeled_category
+        name of the unlabeled cells in the label_key.
+
+    Returns
+    ----------
+    Anndata object integrated with predicted annotation.
+    """
+    scvi.model.SCVI.setup_anndata(adata_train, 
+                                  layer=layer, 
+                                  labels_key=label_key,
+                                  batch_key=batch_key)
+    vae = scvi.model.SCVI(adata_train, 
+                          n_layers=n_layers, 
+                          n_latent=n_latent)
+    vae.train()
+    print('Done scVI')
+    
+    # scvi.model.SCANVI.setup_anndata(concat, layer=layer, batch_key="tech",
+    #                                 labels_key=label_key, unlabeled_category="nan")
+    # pas trop utile on l'a deja fixe pour scvi -> garde meme chose
+    
+    lvae = scvi.model.SCANVI.from_scvi_model(vae, 
+                                             unlabeled_category=unlabeled_category,
+                                             labels_key=label_key,
+                                             adata=adata_train)
+    lvae.train(max_epochs=20, n_samples_per_label=100)
+    print('Done scANVI')
+    
+    concat.obs["C_scANVI"] = lvae.predict(concat)
+    concat.obsm["X_scANVI"] = lvae.get_latent_representation(concat)
+    
+    # add score
+    df_soft = lvae.predict(concat, soft=True) 
+    concat.obs["score"] = df_soft.max(axis=1)
+
+    return concat
+
+
+
+def scanvi_annotate(
+    sdata: 'sd.SpatialData',
+    ad_ref: 'an.AnnData',
+    table: 'str' = 'table',
+    label_key: 'str' = "celltype",
+    batch_categories: 'list' = ["reference", "query"], # batch_categories = ["single_cell", "spatial"]
+    unlabeled_category: 'str' = "unknown",
+    batch_key: 'str' = "tech",
+    layer: 'str' = "counts",
+    path_to_renamed_genes: 'str' = '/data/analysis/data_fierville/SPATIAL/BPCO/Human_GRCh38_renamed_genes.yaml',
+    metaref2add: 'list' = [],
+    filter_under_score: 'float | None' = 0.5,
+    n_layers: 'int' = 2,
+    n_latent: 'int' = 50,
+) -> 'an.AnnData':
+    """Make the transfert label from the single-cell dataset to the spatial dataset.
+
+    Parameters
+    ----------
+    sdata
+        SpatialData object. 
+    ad_ref
+        Anndata single-cell reference object.
+    table
+        table to use in the spatial object to make the integration.
+    label_key
+        .obs key in single-cell reference object and in spatial object.
+    batch_categories
+        name of the two datasets : first name have to be the annotated 
+        dataset and the second the unlabeled.
+    unlabeled_category
+        name of the unlabeled cells in the label_key.
+    batch_key
+        .obs key in anndata object that separate the reference from the query.
+    layer
+        layer in which we can find the raw count values.
+    path_to_renamed_genes
+        path to YAML file with old gene name and new gene name.
     metaref2add
         .obs key in single-cell reference object to transfert to spatial.
     filter_under_score
         remove cells having a scvi assignment score under this cutoff
+    n_layers
+        number of hidden layers used for encoder and decoder NNs.
+    n_latent
+        dimensionality of the latent space.
 
+    Returns
+    ----------
+    Anndata object with the two datasets concatenate.
     """
-    ad_spatial.var.index = ad_spatial.var.index.str.upper()
-    ad_ref.var.index = ad_ref.var.index.str.upper()
+    ad_spatial = sdata[table]
+    
+    if layer not in ad_ref.layers.keys():
+        ad_ref.layers[layer] = ad_ref.raw.X.copy()
 
-    print("ref. ", ad_ref.shape)
-    print("viz. ", ad_spatial.shape)
-
-    # Select shared gene panel genes only
-    genes_Vizgen = ad_spatial.var.index
-    genes_10x = ad_ref.var.index
-    genes_shared = genes_Vizgen.intersection(genes_10x)  # List of shared genes
-    ad_emb = ad_spatial[:, genes_Vizgen.isin(genes_shared)].copy()
-    ad_ref = ad_ref[:, genes_10x.isin(genes_shared)]
-
-    print(len(genes_shared), "common genes")
-
-    # missed = list(set(genes_Vizgen) - set(genes_shared))
-    # print("gene missed = ", missed)
-
-    # Concatenate the datasets
-    # both needs to have the count values in the layer "counts" or layer received
-    concat = ad_ref.concatenate(ad_emb, batch_key="tech", batch_categories=["10x", "MERFISH"]).copy()
-    # concat.layers[layer] = concat.raw.X.copy()
-
-    # Use the annotations from the 10x, and treat the MERFISH as unlabeled
-    concat.obs[f"{label_key}"] = "nan"
-    mask = concat.obs["tech"] == "10x"
-    concat.obs[f"{label_key}"][mask] = ad_ref.obs[label_ref].values
-
-    # Create the scVI latent space
-    scvi.model.SCVI.setup_anndata(concat, layer=layer, batch_key="tech")
-    vae = scvi.model.SCVI(concat)
-    # Train the model
-    vae.train()
-
-    # Register the object and run scANVI
-    scvi.model.SCANVI.setup_anndata(
-        concat,
-        layer=layer,
-        batch_key="tech",
-        labels_key=label_key,
-        unlabeled_category="nan",
+    concat = prepareToScvi(
+        ad_spatial=ad_spatial,
+        ad_ref=ad_ref,
+        label_key = label_key,
+        batch_categories = batch_categories,
+        unlabeled_category = unlabeled_category,
+        batch_key = batch_key,
+        layer = layer,
+        path_to_renamed_genes = path_to_renamed_genes
+    )
+   
+    concat = run_scANVI(
+        adata_train=concat,
+        layer = layer,
+        batch_key = batch_key,
+        label_key = label_key,
+        n_layers = n_layers,
+        n_latent = n_latent,
+        unlabeled_category = unlabeled_category
     )
 
-    lvae = scvi.model.SCANVI.from_scvi_model(vae, labels_key=label_key, unlabeled_category="nan", adata=concat)
-    lvae.train(max_epochs=20, n_samples_per_label=100)
-
-    concat.obs["C_scANVI"] = lvae.predict(concat)
-    concat.obsm["X_scANVI"] = lvae.get_latent_representation(concat)
-
-    # add score
-    df_soft = lvae.predict(concat, soft=True)
-    concat.obs["score"] = df_soft.max(axis=1)
-
-    merfish_mask = concat.obs["tech"] == "MERFISH"
-    ad_spatial.obs[f"{label_key}"] = concat.obs["C_scANVI"][merfish_mask].values
-    ad_spatial.obs[f"{label_key}_score"] = concat.obs["score"][merfish_mask].values
-
+    mask_to_predict = concat.obs[batch_key] == batch_categories[1] # 'query'
+    ad_spatial.obs[[f"{label_key}", f"{label_key}_score"]] = concat.obs[["C_scANVI", "score"]][mask_to_predict].values
     ad_spatial.obs[f"{label_key}"] = ad_spatial.obs[f"{label_key}"].astype("category")
 
     for i in range(0, len(metaref2add)):
-        d = pd.Series(ad_ref.obs[f"{metaref2add[i]}"].values, index=ad_ref.obs[f"{label_ref}"]).to_dict()
+        d = pd.Series(ad_ref.obs[f"{metaref2add[i]}"].values, 
+                      index=ad_ref.obs[f"{label_key}"]).to_dict()
         ad_spatial.obs[f"{metaref2add[i]}"] = ad_spatial.obs[f"{label_key}"].map(d)
         ad_spatial.obs[f"{metaref2add[i]}"] = ad_spatial.obs[f"{metaref2add[i]}"].astype("category")
+    
+    if filter_under_score:
+        print(f'Remove cells with a score < {filter_under_score}...')
+        nb_cells = ad_spatial.n_obs
+        ad_spatial = ad_spatial[ad_spatial.obs[f"{label_key}_score"] >= filter_under_score]
+        print(f'Low assignment score filtering {nb_cells - ad_spatial.n_obs} cells.')
 
-    # remove cells having a bad score
-    nb_cells = ad_spatial.shape[0]
-    ad_spatial = ad_spatial[ad_spatial.obs[f"{label_key}_score"] >= filter_under_score]
-    filtered_cells = nb_cells - ad_spatial.shape[0]
-    print("low assignment score filtering ", filtered_cells)
-
+    return 
 
 # def switch_region(
 #    sdata: sd.SpatialData,
